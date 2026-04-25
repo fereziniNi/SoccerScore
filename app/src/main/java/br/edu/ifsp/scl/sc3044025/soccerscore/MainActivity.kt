fed8141a62b35586182e0a576c4990945ecef5ab
fereziniNi <nikinhaferezini14@gmail.com>
2026-04-25 10:02:28 -0300

    feat: add input to soccer team name

--- app/src/main/java/br/edu/ifsp/scl/sc3044025/soccerscore/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3044025/soccerscore/MainActivity.kt
@@ -7,9 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import br.edu.ifsp.scl.sc3044025.soccerscore.ui.theme.SoccerScoreTheme
@@ -32,6 +37,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun ConfigureSoccerMatch(modifier: Modifier){
+    var soccerTeamA by rememberSaveable { mutableStateOf("") }
+    var soccerTeamB by rememberSaveable { mutableStateOf("") }
+
+
     Column(
         modifier = modifier.fillMaxSize(),
         horizontalAlignment = androidx.compose.ui.Alignment.CenterHorizontally
@@ -39,6 +48,19 @@ fun ConfigureSoccerMatch(modifier: Modifier){
         Text(
             text = "Soccer Match"
         )
+
+        OutlinedTextField(
+            value = soccerTeamA,
+            onValueChange = { soccerTeamA = it },
+            label = { Text("Soccer team name A") }
+        )
+
+        OutlinedTextField(
+            value = soccerTeamB,
+            onValueChange = { soccerTeamB = it },
+            label = { Text("Soccer team name B") }
+        )
+
     }
 }
 
